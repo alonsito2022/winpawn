329588f1ec9d3779ebe7ddb8f5591e080f72d2d0
Miguel Alonso <mgl.suarez1@gmail.com>
2024-02-11 17:43:25 -0500

    ll

--- Views/Dashboard.Designer.vb
+++ Views/Dashboard.Designer.vb
@@ -1,84 +1,76 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Dashboard
-    Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+    Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        If disposing AndAlso components IsNot Nothing Then
-            components.Dispose()
-        End If
-        MyBase.Dispose(disposing)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
     End Sub
 
-    'Required by the Windows Form Designer
+    'Requerido por el Diseñador de Windows Forms
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
-        Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
-        Me.Administracion = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
-        Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
-        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.LateralMenu = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LateralMenu.SuspendLayout()
         Me.SuspendLayout()
         '
-        'RibbonControl
+        'LateralMenu
         '
-        Me.RibbonControl.ExpandCollapseItem.Id = 0
-        Me.RibbonControl.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.RibbonControl.ExpandCollapseItem, Me.RibbonControl.SearchEditItem})
-        Me.RibbonControl.Location = New System.Drawing.Point(0, 0)
-        Me.RibbonControl.MaxItemId = 1
-        Me.RibbonControl.Name = "RibbonControl"
-        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
-        Me.RibbonControl.Size = New System.Drawing.Size(1161, 158)
-        Me.RibbonControl.StatusBar = Me.RibbonStatusBar
+        Me.LateralMenu.BackColor = System.Drawing.Color.FromArgb(CType(CType(11, Byte), Integer), CType(CType(7, Byte), Integer), CType(CType(17, Byte), Integer))
+        Me.LateralMenu.Controls.Add(Me.Button1)
+        Me.LateralMenu.Controls.Add(Me.Panel1)
+        Me.LateralMenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.LateralMenu.Location = New System.Drawing.Point(0, 0)
+        Me.LateralMenu.Name = "LateralMenu"
+        Me.LateralMenu.Size = New System.Drawing.Size(200, 649)
+        Me.LateralMenu.TabIndex = 0
         '
-        'RibbonPage1
+        'Panel1
         '
-        Me.RibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.Administracion})
-        Me.RibbonPage1.Name = "RibbonPage1"
-        Me.RibbonPage1.Text = "Administracion"
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 90)
+        Me.Panel1.TabIndex = 0
         '
-        'Administracion
+        'Button1
         '
-        Me.Administracion.Name = "Administracion"
-        Me.Administracion.Text = "Administracion"
-        '
-        'RibbonStatusBar
-        '
-        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 655)
-        Me.RibbonStatusBar.Name = "RibbonStatusBar"
-        Me.RibbonStatusBar.Ribbon = Me.RibbonControl
-        Me.RibbonStatusBar.Size = New System.Drawing.Size(1161, 24)
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button1.Location = New System.Drawing.Point(0, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 45)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Dashboard
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1161, 679)
-        Me.Controls.Add(Me.RibbonStatusBar)
-        Me.Controls.Add(Me.RibbonControl)
-        Me.IsMdiContainer = True
+        Me.ClientSize = New System.Drawing.Size(1175, 649)
+        Me.Controls.Add(Me.LateralMenu)
         Me.Name = "Dashboard"
-        Me.Ribbon = Me.RibbonControl
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.StatusBar = Me.RibbonStatusBar
         Me.Text = "Dashboard"
-        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.LateralMenu.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents RibbonControl As DevExpress.XtraBars.Ribbon.RibbonControl
-    Friend WithEvents RibbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
-    Friend WithEvents Administracion As DevExpress.XtraBars.Ribbon.RibbonPageGroup
-    Friend WithEvents RibbonStatusBar As DevExpress.XtraBars.Ribbon.RibbonStatusBar
-
-
+    Friend WithEvents LateralMenu As Panel
+    Public WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
 End Class
